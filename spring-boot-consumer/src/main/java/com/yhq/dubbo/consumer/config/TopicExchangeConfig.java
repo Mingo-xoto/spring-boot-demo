62bca07662fa3c583ef1e7dee30dc9d46755c2a8
faker <541871045@qq.com>
2017-06-14 18:18:23 +0800

    --

--- spring-boot-consumer/src/main/java/com/yhq/dubbo/consumer/config/TopicExchangeConfig.java
+++ spring-boot-consumer/src/main/java/com/yhq/dubbo/consumer/config/TopicExchangeConfig.java
@@ -2,10 +2,8 @@ package com.yhq.dubbo.consumer.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-//@Profile("rabbit-mq")
 public class TopicExchangeConfig {
 
 	@Value("${topicExchange.routingKey}")
