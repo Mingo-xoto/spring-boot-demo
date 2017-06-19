84b4ebb5bc0610e736e029bb4f7633da1fe294b4
faker <541871045@qq.com>
2017-06-19 13:39:06 +0800

    。。

--- spring-boot-consumer/src/main/java/com/yhq/dubbo/consumer/config/WebConfig.java
+++ spring-boot-consumer/src/main/java/com/yhq/dubbo/consumer/config/WebConfig.java
@@ -1,15 +1,46 @@
-package com.yhq.dubbo.consumer.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-@Configuration
-// @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/default").setViewName("default");
-	}
-}
+//package com.yhq.dubbo.consumer.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+//import org.springframework.web.servlet.view.xml.MappingJackson2XmlView;
+//
+//@Configuration
+//public class WebConfig extends WebMvcConfigurerAdapter {
+//
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry registry) {
+//		registry.addViewController("/default").setViewName("default");
+//	}
+//
+//	/**
+//	 * 视图解析器
+//	 */
+//	public void configureViewResolvers(ViewResolverRegistry registry) {
+//		registry.jsp("/jsp/", ".jsp");
+//		// 启用内容裁决视图解析器
+//		registry.enableContentNegotiation(new MappingJackson2JsonView(), new MappingJackson2XmlView());
+//	};
+//
+//	@Override
+//	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//		/* 是否通过请求Url的扩展名来决定media type */
+//		configurer.favorPathExtension(true)
+//				/* 不检查Accept请求头 */
+//				.ignoreAcceptHeader(true)
+//				/* 请求类型参数名称 */
+//				.parameterName("mediaType")
+//				/* 设置默认的media yype */
+//				.defaultContentType(MediaType.TEXT_HTML)
+//				/* 请求以.html结尾的会被当成MediaType.TEXT_HTML */
+//				.mediaType("html", MediaType.TEXT_HTML)
+//				/* 请求以.xml结尾的会被当成MediaType.APPLICATION_XML */
+//				.mediaType("xml", MediaType.APPLICATION_XML)
+//				/* 请求以.json结尾的会被当成MediaType.APPLICATION_JSON */
+//				.mediaType("json", MediaType.APPLICATION_JSON);
+//	}
+//}
